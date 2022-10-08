7af21a3b369cbbead1e2058f7880c17edd8d638a
juliaingles <ingleslopez.julia@gmail.com>
2022-10-08 11:36:54 +0200

    index version master

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-//index
-?>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>web</title>
+  <LINK href="styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<!-- Feel free to change this text here -->
+<p>
+  Rama master
+</p>
+<p>
+</p>
+
+</body>
+</html>
