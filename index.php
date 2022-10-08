b00723794c2edb970383fb9dc57f2447468b7ad4
juliaingles <ingleslopez.julia@gmail.com>
2022-10-08 11:18:24 +0200

    Primer commit en test_index

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-//index
-?>
+
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>web</title>
+  <LINK href="styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<!-- Feel free to change this text here -->
+<p>
+  
+</p>
+<p>
+</p>
+
+</body>
+</html>
